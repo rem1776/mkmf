fc2c340be71c2eba03a095f622ea6b640568cd03
Ryan Mulhall <Ryan.Mulhall@gaea51.ncrc.gov>
2026-01-21 11:32:26 -0500

    set the hpcme pkg config path via the makefile

--- templates/hpcme-intel25-oneapi.mk
+++ templates/hpcme-intel25-oneapi.mk
@@ -11,6 +11,10 @@ CC = mpiicx
 LD = /opt/intel/oneapi/compiler/latest/bin/compiler/lld
 AR = /opt/intel/oneapi/compiler/latest/bin/compiler/llvm-ar
 
+# TODO this should be set in the hpcme dockerfiles instead
+PKG_CONFIG_PATH = "${PKG_CONFIG_PATH}:/opt/spack-environment/.spack-env/view/lib/pkgconfig/"
+export PKG_CONFIG_PATH
+
 #######################
 # Build target macros
 #
