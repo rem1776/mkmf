9bea917d53ca6fba4feaec954159cdc05ac2f405
Ryan Mulhall <Ryan.Mulhall@gaea51.ncrc.gov>
2026-01-21 12:23:58 -0500

    use different linker(for some reason?) and another try at setting pkg_config_path

--- templates/hpcme-intel25-oneapi.mk
+++ templates/hpcme-intel25-oneapi.mk
@@ -8,12 +8,11 @@
 ############
 FC = mpiifx
 CC = mpiicx
-LD = /opt/intel/oneapi/compiler/latest/bin/compiler/lld
+LD = /opt/intel/oneapi/compiler/latest/bin/compiler/ld.lld
 AR = /opt/intel/oneapi/compiler/latest/bin/compiler/llvm-ar
 
 # TODO this should be set in the hpcme dockerfiles instead
-PKG_CONFIG_PATH = "/opt/spack-environment/.spack-env/view/lib/pkgconfig/"
-export PKG_CONFIG_PATH
+export PKG_CONFIG_PATH = "/opt/spack-environment/.spack-env/view/lib/pkgconfig/"
 
 #######################
 # Build target macros
